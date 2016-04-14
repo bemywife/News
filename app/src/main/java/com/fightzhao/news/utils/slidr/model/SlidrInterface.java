4e25e1ec96919b6f5dbd881c1d59830b33f4ab13
fightzhao <fightzhao@fightzhaodeMacBook-Pro.local>
2016-04-14 16:13:01 +0800

    Initial commit

---
+++ app/src/main/java/com/fightzhao/news/utils/slidr/model/SlidrInterface.java
@@ -0,0 +1,13 @@
+package com.fightzhao.news.utils.slidr.model;
+
+/**
+ * Created by fightzhao on 16/3/24.
+ * 控制侧滑的开关
+ */
+public interface SlidrInterface {
+
+    void lock();
+
+    void unlock();
+
+}
